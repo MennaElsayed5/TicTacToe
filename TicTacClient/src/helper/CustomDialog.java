bbd2d43cea513ea92367547e9762e093ca8cb281
Eslam Esmael <EslamEsmael012@Gmail.com>
2021-12-26 21:43:58 +0200

    Adding play again dialog

--- TicTacClient/src/helper/CustomDialog.java
+++ TicTacClient/src/helper/CustomDialog.java
@@ -11,23 +11,21 @@ import javafx.scene.control.ButtonType;
 
 /**
  *
- * @author Ghaly
+ * @author Eslam Esmael
  */
 public class CustomDialog {
-    
-    public static boolean askPlayAgain(String s)
-    {   
-        
-        ButtonType Yes = new ButtonType("Play Again"); 
+
+    public static boolean askPlayAgain(String s) {
+        ButtonType Yes = new ButtonType("Play Again");
         ButtonType No = new ButtonType("NO", ButtonBar.ButtonData.CANCEL_CLOSE);
-        Alert a = new Alert(Alert.AlertType.NONE); 
+        Alert a = new Alert(Alert.AlertType.NONE);
         a.setTitle("Play Again");
-        a.getDialogPane().getButtonTypes().addAll(Yes,No);
+        a.getDialogPane().getButtonTypes().addAll(Yes, No);
         a.setHeaderText(s);
-         a.showAndWait();
-        if(a.getResult()==Yes){  
-             return true; 
-        }else{
+        a.showAndWait();
+        if(a.getResult() == Yes) {
+            return true;
+        } else {
             return false;
         }
     }
